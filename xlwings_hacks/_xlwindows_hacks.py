59f0e95c8b58f58ae556de194445cd90def2b4f3
junkmd <jun.k.job.finding12@gmail.com>
2019-02-10 10:24:23 +0900

    add QueryTables and QueryTable.

--- xlwings_hacks/_xlwindows_hacks.py
+++ xlwings_hacks/_xlwindows_hacks.py
@@ -1,8 +1,10 @@
 '''xlwings hacks'''
 import xlwings._xlwindows as xlwindows
+from abc import ABC, ABCMeta, abstractmethod
 
 
-class ListObject(object):
+class BaseTable(object):
+    """internal class."""
     def __init__(self, xl):
         self.xl = xl
 
@@ -26,11 +28,15 @@ class ListObject(object):
     def parent(self):
         return Sheet(xl=self.xl.Parent)
 
+    def refresh(self):
+        self.xl.Refresh()
 
-class ListObjects(xlwindows.Collection):
 
-    _wrap = ListObject
-
+class BaseTables(xlwindows.Collection):
+    """
+    internal class.
+    _wrap attribute must be not None.
+    """
     def __init__(self, xl):
         xlwindows.Collection.__init__(self, xl)
 
@@ -39,29 +45,72 @@ class ListObjects(xlwindows.Collection):
         return xlwindows.Sheet(xl=self.xl.Parent)
 
 
-def _sheet_attr_listobjects(sheet):
+def _attr_tables(py_class, xl_obj):
     """
-    Returns the listobjects implement of the sheet.
+    internal function.
+    Returns the tables implement of the object.
 
     Arguments
     ---------
-    sheet : xlwings.Sheet.impl or xlwings.xlplatform.Sheet
+    py_class : Class of implement.
+    xl_obj : win32com object.
 
     Examples
     --------
 
     .. code-block:: python
 
-        import xlwings as xw
-
-        class SubclassOfSheet(xw.Sheet):
-            @property
-            def listobjects(self):
-                return _sheet_attr_listobjects(self.impl)
-
-        wb = xw.Book()
-        ws = SubclassOfSheet(impl=wb.sheets[0].impl)
-        lo = ws.listobjects
-
+        def _attr_listobjects(obj):
+            return _attr_tables(ListObjects, obj.xl.ListObjects)
     """
-    return ListObjects(sheet.xl.ListObjects)
+    return py_class(xl_obj)
+
+
+class ListObject(BaseTable):
+    @property
+    def querytable(self):
+        return QueryTable(self.xl.QueryTable)
+
+
+class ListObjects(BaseTables):
+    _wrap = ListObject
+
+
+def _attr_listobjects(obj):
+    return _attr_tables(ListObjects, obj.xl.ListObjects)
+
+
+class QueryTable(BaseTable):
+    def __init__(self, xl):
+        BaseTable.__init__(self, xl)
+        self.background_query = False
+
+    @property
+    def background_query(self):
+        return self.xl.BackgroundQuery
+
+    @background_query.setter
+    def background_query(self, value):
+        self.xl.BackgroundQuery = value
+
+    @property
+    def command_text(self):
+        return self.xl.CommandText
+
+    @command_text.setter
+    def command_text(self, text):
+        self.xl.CommandText = text
+
+
+class QueryTables(BaseTables):
+    _wrap = QueryTable
+
+
+def _attr_querytables(obj):
+    return _attr_tables(QueryTables, obj.xl.QueryTables)
+
+
+if __name__ == '__main__':
+    import xlwings as xw
+
+    pass
--- xlwings_hacks/main_hacks.py
+++ xlwings_hacks/main_hacks.py
@@ -1,4 +1,3 @@
-import xlwings as xw
 import xlwings.main as xlmain
 import sys
 
@@ -9,7 +8,7 @@ else:
     # import _xlmac_hacks as xlplatform_hacks
 
 
-class ListObject(object):
+class BaseTable(object):
     def __init__(self, impl=None):
         self.impl = impl
 
@@ -24,21 +23,22 @@ class ListObject(object):
     @property
     def parent(self):
         """
-        Returns the parent of the listobject.
+        Returns the parent of the object.
         """
         return Sheet(impl=self.impl.parent)
 
     @property
     def name(self):
         """
-        Returns or sets the name of the listobject.
+        Returns or sets the name of the object.
         """
         return self.impl.name
 
+    def refresh(self):
+        self.impl.refresh()
 
-class ListObjects(xlmain.Collection):
-    _wrap = ListObject
 
+class BaseTables(xlmain.Collection):
     @property
     def parent(self):
         return xlmain.Sheet(impl=self.impl.parent)
@@ -52,46 +52,86 @@ class ListObjects(xlmain.Collection):
         return self.impl.api
 
 
-class SheetWithListObject(xw.Sheet):
+class ListObject(BaseTable):
+    @property
+    def querytable(self):
+        return QueryTable(self.impl.querytable)
+
+
+class ListObjects(BaseTables):
+    _wrap = ListObject
+
+
+class SheetWithListObject(xlmain.Sheet):
     @property
     def listobjects(self):
         """
         Returns a ListObjects.
         """
         return ListObjects(
-            xlplatform_hacks._sheet_attr_listobjects(
+            xlplatform_hacks._attr_listobjects(
                 self.impl))
+
+
+class QueryTable(BaseTable):
+    @property
+    def background_query(self):
+        return self.impl.background_query
+
+    @background_query.setter
+    def background_query(self, value):
+        self.impl.background_query = value
+
+    @property
+    def command_text(self):
+        return self.impl.command_text
+
+    @command_text.setter
+    def command_text(self, text):
+        self.impl.command_text = text
+
+
+class QueryTables(BaseTables):
+    _wrap = QueryTable
 
 
 if __name__ == '__main__':
     import xlwings as xw
 
+    CONN_STR = \
+        "ODBC;"\
+        "DSN=MS Access Database;"\
+        "DBQ=C:\python\Access2003.mdb;"\
+        "DefaultDir=C:\python\Access2003.mdb;"\
+        "DriverId=25;"\
+        "FIL=MS Access;"\
+        "MaxBufferSize=2048;"\
+        "PageTimeout=5;"
+
+    sqlstr = "SELECT * FROM 顧客名"
+
     wb = xw.Book()
-    ws = SheetWithListObject(impl=wb.sheets[0].impl)
+
+    ws = SheetWithListObject(impl=wb.sheets.add().impl)
     los = ws.listobjects
     rng = ws.range((1, 1))
 
-    print(len(los))
-
     myListObject = ws.api.ListObjects.Add(
         SourceType=0,
-        Source="ODBC;"
-        "DSN=MS Access Database;"
-        "DBQ=C:\python\Access2003.mdb;"
-        "DefaultDir=C:\python\Access2003.mdb;"
-        "DriverId=25;"
-        "FIL=MS Access;"
-        "MaxBufferSize=2048;"
-        "PageTimeout=5;",
+        Source=CONN_STR,
         LinkSource=True,
         Destination=rng.api)
-    myQueryTable = myListObject.QueryTable
 
-    myQueryTable.CommandText = "SELECT * FROM 顧客名"
+    lo = los[0]
+    lo.querytable.command_text = sqlstr
+    lo.refresh()
 
-    myQueryTable.Refresh()
+    ws = wb.sheets.add()
+    qts = QueryTables(xlplatform_hacks._attr_querytables(ws.impl))
+    rng = ws.range((1, 1))
 
-    for lo in los:
-        print(lo.name)
+    myQueryTable = ws.api.QueryTables.Add(
+        CONN_STR, rng.api, sqlstr)
 
-    print(los[0].name)
+    qt = qts[0]
+    qt.refresh()
